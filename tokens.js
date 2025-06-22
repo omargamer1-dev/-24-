b4decce7f69b1b9492ceed192df64eddaf14c987
omargamer1-dev <xhzhzjz1060@gmail.com>
2025-06-22 08:06:59 +0300

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,18 @@
+// status can be "online", "idle", "dnd", or "invisible" or "offline"
+export default [
+    {
+        channelId: "1359942136467820785",
+        serverId: "1133285493778436136",
+        token: process.env.token1,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: true,
+    },
+];
